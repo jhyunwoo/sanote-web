3f85aee2cf78c38d377af00c874b9c018280ebc9
jhyunwoo <jhyunwoo0228@gmail.com>
2023-04-27 12:29:49 +0900

    get class list

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -4,12 +4,15 @@ import pb from "@/lib/pocketbase"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 
+type ClassListType = any
+
 export default function Teachers() {
   const date = new Date()
   const [year, setYear] = useState<Number>()
-  const [semester, setSemester] = useState<Number>()
+  const [semister, setSemister] = useState<Number>()
+  const [classes, setClasses] = useState<ClassListType[]>([])
 
-  function checkSemester(month: number) {
+  function checkSemister(month: number) {
     if (month <= 7) {
       return 1
     } else {
@@ -19,19 +22,39 @@ export default function Teachers() {
 
   useEffect(() => {
     async function getClassList() {
+      setYear(date.getFullYear())
+      setSemister(checkSemister(date.getMonth()))
       const records = await pb.collection("classes").getFullList({
-        filter: `year=${date.getFullYear()}`,
+        filter: `year=${date.getFullYear()}&&semister=${checkSemister(
+          date.getMonth(),
+        )}`,
       })
-      console.log(records)
+      setClasses(records)
     }
     getClassList()
   }, [])
+
+  useEffect(() => {
+    async function getClassList(filter: string) {
+      const records = await pb.collection("classes").getFullList({
+        filter: filter,
+      })
+      setClasses(records)
+    }
+    if (year && !semister) {
+      getClassList(`year=${year}`)
+    } else if (!year && semister) {
+      getClassList(`semister=${semister}`)
+    } else if (year && semister) {
+      getClassList(`year=${year}&&semister=${semister}`)
+    }
+  }, [year, semister])
 
   return (
     <div>
       <div>Teacher Page</div>
       <Link href={"/teachers/create-class"}>Create Class</Link>
-      <div className="flex flex-col">
+      <div className="flex justify-around w-full">
         <button onClick={() => setYear(date.getFullYear() - 1)}>
           {date.getFullYear() - 1}
         </button>
@@ -43,10 +66,23 @@ export default function Teachers() {
         </button>
       </div>
       <div>
-        <button onClick={() => setSemester(1)}>1학기</button>
-        <button onClick={() => setSemester(2)}>2학기</button>
+        <button onClick={() => setSemister(1)}>1학기</button>
+        <button onClick={() => setSemister(2)}>2학기</button>
       </div>
-      <div></div>
+      <div>
+        {classes.map((data, key) => (
+          <div
+            key={key}
+            className="bg-slate-50 p-4 m-2 rounded-xl flex flex-col"
+          >
+            <div>
+              {data.name} ({year} {semister}학기)
+            </div>
+            <div>{data.pac}팩</div>
+            <div>{data?.students?.length}명</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
