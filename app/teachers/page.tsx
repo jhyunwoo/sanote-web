849ea6fa43e5f5c4b42b5e6e06aa2d545f5c06c0
jhyunwoo <jhyunwoo0228@gmail.com>
2023-04-27 11:29:41 +0900

    get class list

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -1,27 +1,52 @@
 "use client"
 
-import { useForm, SubmitHandler } from "react-hook-form"
-
-type Inputs = {
-  year: number
-  semester: number
-}
-
+import pb from "@/lib/pocketbase"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 export default function Teachers() {
-  const year = new Date().getFullYear()
+  const date = new Date()
+  const [year, setYear] = useState<Number>()
+  const [semester, setSemester] = useState<Number>()
+
+  function checkSemester(month: number) {
+    if (month <= 7) {
+      return 1
+    } else {
+      return 2
+    }
+  }
+
+  useEffect(() => {
+    async function getClassList() {
+      const records = await pb.collection("classes").getFullList({
+        filter: `year=${date.getFullYear()}`,
+      })
+      console.log(records)
+    }
+    getClassList()
+  }, [])
+
   return (
     <div>
       <div>Teacher Page</div>
       <Link href={"/teachers/create-class"}>Create Class</Link>
       <div className="flex flex-col">
-        <button>{year - 2}</button>
-        <button>{year - 1}</button>
-        <button>{year}</button>
-        <button>{year + 1}</button>
-        <button>{year + 2}</button>
+        <button onClick={() => setYear(date.getFullYear() - 1)}>
+          {date.getFullYear() - 1}
+        </button>
+        <button onClick={() => setYear(date.getFullYear())}>
+          {date.getFullYear()}
+        </button>
+        <button onClick={() => setYear(date.getFullYear() + 1)}>
+          {date.getFullYear() + 1}
+        </button>
       </div>
+      <div>
+        <button onClick={() => setSemester(1)}>1학기</button>
+        <button onClick={() => setSemester(2)}>2학기</button>
+      </div>
+      <div></div>
     </div>
   )
 }
