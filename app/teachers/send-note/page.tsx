03073f9c6e512593ed9f51ec4d86df54a4b7e40d
jhyunwoo <jhyunwoo0228@gmail.com>
2023-04-28 15:16:15 +0900

    add search system

--- app/teachers/send-note/page.tsx
+++ app/teachers/send-note/page.tsx
@@ -1,10 +1,13 @@
 "use client"
 import pb from "@/lib/pocketbase"
 import { useForm, SubmitHandler } from "react-hook-form"
+import { useState } from "react"
 
 type Inputs = {
   search: string
 }
+
+type Any = any
 
 export default function SendNote() {
   const {
@@ -13,11 +16,21 @@ export default function SendNote() {
     formState: { errors },
   } = useForm<Inputs>()
 
+  const [searchResult, setSearchResult] = useState<Any[]>([])
+
   const onSubmit: SubmitHandler<Inputs> = async data => {
-    const record = await pb.collection("users").getFullList({
-      filter: `studentId~${data.search} || name~${data.search}`,
-    })
-    console.log(record)
+    try {
+      parseInt(data.search)
+      const record = await pb.collection("users").getFullList({
+        filter: `studentId=${Number(data.search)}`,
+      })
+      setSearchResult(record)
+    } catch {
+      const record = await pb.collection("users").getFullList({
+        filter: `name~"${data.search}"`,
+      })
+      setSearchResult(record)
+    }
   }
 
   return (
@@ -29,6 +42,16 @@ export default function SendNote() {
           <input {...register("search")} />
           <button type="submit">검색</button>
         </form>
+      </div>
+      <div>
+        {searchResult.map((data, key) => (
+          <div key={key} className="flex w-full justify-around">
+            <div>{data.name}</div>
+            <div>{data.studentId}</div>
+            <div>{data.year}학년</div>
+            <div>{data.class}반</div>
+          </div>
+        ))}
       </div>
     </div>
   )
