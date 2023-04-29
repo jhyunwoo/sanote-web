52e1296f5a6b94eeffde013675a4ac06efd16509
전현우 <jhyunwoo@moveto.local>
2023-04-29 16:49:17 +0900

    push function with 3 options

--- app/teachers/send-note/page.tsx
+++ app/teachers/send-note/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import pb from "@/lib/pocketbase"
 import { useForm, SubmitHandler } from "react-hook-form"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 type SearchType = {
@@ -23,22 +23,44 @@ type SenderType = {
   class: number
 }
 
+type PushTypeType = string
+
+type ClassListType = {
+  name: string
+  pac: number
+  semister: number
+  year: number
+  students: string[]
+  id: string
+}
+
 export default function SendNote() {
+  const [pushType, setPushType] = useState<PushTypeType>("")
+
+  const [classList, setClassList] = useState<ClassListType[]>([])
+
+  // 검색 React Hook Form 설정
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<SearchType>()
 
+  // Push React Hook Form 설정
   const {
     register: register2,
     handleSubmit: handleSubmit2,
     formState: { errors: errors2 },
   } = useForm<PushType>()
 
+  // 검색 결과 리스트
   const [searchResult, setSearchResult] = useState<Any[]>([])
+  // 보내는 사람 리스트
   const [sender, setSender] = useState<SenderType[]>([])
 
+  const [classSender, setClassSender] = useState<ClassListType[]>([])
+
+  /** 검색 후 리스트 저장 */
   const onSubmit: SubmitHandler<SearchType> = async data => {
     try {
       parseInt(data.search)
@@ -54,19 +76,35 @@ export default function SendNote() {
     }
   }
 
+  /** 사용자로부터 받은 메세지 정보를 전달하고 메세지 전송 */
   const sendPush: SubmitHandler<PushType> = async data => {
-    if (sender.length > 0) {
-      console.log(data)
-      await axios.post("/api/send-push", {
-        userInfo: sender,
+    if (pushType === "individual") {
+      if (sender.length > 0) {
+        await axios.post("/api/send-push", {
+          userInfo: sender,
+          push: { title: data.title, message: data.message },
+        })
+      } else {
+        alert("보낼 사람을 선택하세요")
+      }
+    } else if (pushType === "announcement") {
+      if (classSender.length > 0) {
+        await axios.post("/api/send-push-announcement", {
+          userInfo: classSender,
+          push: { title: data.title, message: data.message },
+        })
+      } else {
+        alert("보낼 사람을 선택하세요")
+      }
+    } else if (pushType === "emergency") {
+      await axios.post("/api/send-push-emergency", {
         push: { title: data.title, message: data.message },
       })
-    } else {
-      alert("보낼 사람을 선택하세요")
     }
   }
 
-  function addList(userInfo: any) {
+  /** 보내는 사람 리스트에 사람 추가 */
+  function addSender(userInfo: any) {
     let isInclude = false
     for (let i = 0; i < sender.length; i++) {
       if (
@@ -93,6 +131,36 @@ export default function SendNote() {
       ])
   }
 
+  function addClass(classInfo: ClassListType) {
+    let isInclude = false
+    for (let i = 0; i < classSender.length; i++) {
+      if (
+        JSON.stringify({
+          name: classInfo.name,
+          pac: classInfo.pac,
+          semister: classInfo.semister,
+          year: classInfo.year,
+          students: classInfo.students,
+          id: classInfo.id,
+        }).includes(JSON.stringify(classSender[i]))
+      )
+        isInclude = true
+    }
+    if (!isInclude)
+      setClassSender([
+        ...classSender,
+        {
+          name: classInfo.name,
+          pac: classInfo.pac,
+          semister: classInfo.semister,
+          year: classInfo.year,
+          students: classInfo.students,
+          id: classInfo.id,
+        },
+      ])
+  }
+
+  /** 보내는 사람 리스트에서 사람 삭제 */
   function deleteSender(key: number) {
     let senderList = sender
     let newList = []
@@ -105,51 +173,120 @@ export default function SendNote() {
     setSender(newList)
   }
 
-  async function sendNote() {
-    console.log(sender)
-    await axios.post("/api/send-push", { userInfo: sender, message: "hello" })
+  function deleteClassSender(key: number) {
+    let classSenderList = classSender
+    let newList = []
+    let deleteSenderInfo = JSON.stringify(classSender[key])
+    for (let i = 0; i < classSenderList.length; i++) {
+      if (!(JSON.stringify(classSenderList[i]) === deleteSenderInfo)) {
+        newList.push(classSenderList[i])
+      }
+    }
+    setClassSender(newList)
   }
 
   return (
     <div>
       <div>Send Note</div>
-      <div>
-        <div>Search</div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("search")} />
-          <button type="submit">검색</button>
-        </form>
+      <div className="flex w-full justify-around">
+        <button onClick={() => setPushType("individual")}>개별</button>
+        <button
+          onClick={async () => {
+            setPushType("announcement")
+            const classListResult = await pb
+              .collection("classes")
+              .getFullList({ filter: `owner.id="${pb?.authStore?.model?.id}"` })
+            let updateList: ClassListType[] = []
+            classListResult.map(data => {
+              updateList.push({
+                name: data.name,
+                pac: data.pac,
+                semister: data.semister,
+                year: data.year,
+                students: data.students,
+                id: data.id,
+              })
+            })
+            setClassList(updateList)
+          }}
+        >
+          공지
+        </button>
+        <button onClick={() => setPushType("emergency")}>긴급</button>
       </div>
-      <div>
-        {searchResult.map((data, key) => (
-          <button
-            key={key}
-            className="flex w-full justify-around"
-            onClick={() => addList(data)}
-          >
-            <div>{data.name}</div>
-            <div>{data.studentId}</div>
-            <div>{data.year}학년</div>
-            <div>{data.class}반</div>
-          </button>
-        ))}
-      </div>
-
-      <div>
-        <div>List</div>
-        {sender.map((data, key) => (
-          <div key={key} className="flex">
-            <div>{data.name}</div>
-            <button onClick={() => deleteSender(key)}>삭제</button>
+      {pushType === "individual" ? (
+        <div>
+          <div>
+            <div>Search</div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input {...register("search")} />
+              <button type="submit">검색</button>
+            </form>
           </div>
-        ))}
-      </div>
+          <div>
+            {searchResult.map((data, key) => (
+              <button
+                key={key}
+                className="flex w-full justify-around"
+                onClick={() => addSender(data)}
+              >
+                <div>{data.name}</div>
+                <div>{data.studentId}</div>
+                <div>{data.year}학년</div>
+                <div>{data.class}반</div>
+              </button>
+            ))}
+          </div>
+          <div>
+            <div>List</div>
+            {sender.map((data, key) => (
+              <div key={key} className="flex">
+                <div>{data.name}</div>
+                <button onClick={() => deleteSender(key)}>삭제</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {pushType === "announcement" ? (
+        <div>
+          <div>공지</div>
+          <div>
+            {classList.map((data, key) => (
+              <button
+                onClick={() => addClass(data)}
+                key={key}
+                className="flex w-full justify-around"
+              >
+                <div>{data.name}</div>
+                <div>{data.pac}</div>
+                <div>{data.year}</div>
+                <div>{data.students.length}</div>
+              </button>
+            ))}
+          </div>
+          <div>
+            <div>List</div>
+            {classSender.map((data, key) => (
+              <div key={key} className="flex">
+                <div>{data.name}</div>
+                <button onClick={() => deleteClassSender(key)}>삭제</button>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
       <form onSubmit={handleSubmit2(sendPush)}>
         <input {...register2("title", { required: true })} />
         <input {...register2("message", { required: true })} />
         <button type="submit">전송</button>
       </form>
-      {/* <button onClick={sendNote}>쪽지 전송</button> */}
     </div>
   )
 }
---
+++ pages/api/send-push-announcement.ts
@@ -0,0 +1,49 @@
+import pb from "@/lib/pocketbase"
+import type { NextApiRequest, NextApiResponse } from "next"
+import webPush from "web-push"
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (
+    !process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY ||
+    !process.env.WEB_PUSH_EMAIL ||
+    !process.env.WEB_PUSH_PRIVATE_KEY
+  ) {
+    throw new Error("Environment variables supplied not sufficient.")
+  }
+  webPush.setVapidDetails(
+    `mailto:${process.env.WEB_PUSH_EMAIL}`,
+    process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY,
+    process.env.WEB_PUSH_PRIVATE_KEY,
+  )
+  webPush.setGCMAPIKey("1073585647206")
+  const { userInfo, push } = req.body
+  let userList = userInfo
+
+  userList.map(async (data: any) => {
+    data.students.map(async (id: string) => {
+      const records = await pb.collection("pushInfos").getFullList({
+        filter: `user.id="${id}"`,
+      })
+      for (let i = 0; i < records.length; i++) {
+        webPush
+          .sendNotification(
+            {
+              endpoint: records[i].endpoint,
+              keys: {
+                p256dh: records[i].p256dh,
+                auth: records[i].auth,
+              },
+            },
+
+            JSON.stringify({
+              title: push.title,
+              message: push.message,
+              tag: "message-tag",
+            }),
+          )
+          .catch(e => console.log(e))
+      }
+    })
+  })
+  res.status(200)
+}
